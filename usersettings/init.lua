8e9d97f40e36b5eb4af90eb1019a95dbdae323b7
DirkDigglerz <liamsdargie@gmail.com>
2023-05-07 00:45:54 +0100

    Removed any settings that are automatically set and put them in init.lua to save confusion

--- usersettings/init.lua
+++ usersettings/init.lua
@@ -1,3 +1,15 @@
+  --## AUTOMATICALLY SET VARIABLES
+  --## TIME AND WEATHER SYNC
+  Config.TimeSystem = "qb-weathersync", --## vSync or cd_easytime or qb-weathersync
+  --## INVENTORY SYSTEM
+  Config.Inventory         = "ox_inventory", --## "qb-inventory", "mf-inventory", "qs-inventory", "ox_inventory"
+  Config.ItemIconsDir      = "ox_inventory/web/images/", --## Directory for the icons you use for your inventory.
+  --## TARGET
+  Config.TargetSystem = "ox_target", --## qb-target, qtarget, ox_target, dirk-prompts,
+  --## FRAMEWORK OPTION
+  Config.UsingQBCore = true,
+  Config.UsingESX    = false,
+
 local inventories = {
   ['qb-inventory'] = "qb-inventory/html/images/",
   ['qs-inventory'] = "qs-inventory/html/images/",
