ecb0042b36592ad37ba91b2aab7a4ee593dfacaf
DirkDigglerz <liamsdargie@gmail.com>
2023-06-03 01:56:45 +0100

    PolyGon Changes

--- src/client/modules/polygon.lua
+++ src/client/modules/polygon.lua
@@ -74,4 +74,102 @@ Core.Polygon = {
     return plot
   end,
 
+  DrawWall = function(pos1,pos2,height)
+    local r,g,b,a = 0,255,0,54
+    local topLeft     = vector3(pos1.x, pos1.y, pos1.z + height)
+    local bottomLeft  = vector3(pos1.x,pos1.y,pos1.z)
+    local topRight    = vector3(pos2.x,pos2.y,pos2.z + height)
+    local bottomRight = vector3(pos2.x,pos2.y,pos2.z)
+    DrawPoly(bottomLeft,topLeft,bottomRight,r,g,b,a)
+    DrawPoly(topLeft,topRight,bottomRight,r,g,b,a)
+    DrawPoly(bottomRight,topRight,topLeft,r,g,b,a)
+    DrawPoly(bottomRight,topLeft,bottomLeft,r,g,b,a)
+  end,
+
+  newPolygon = function()
+    local polygon = {}
+    local height  = 5.0
+    while true do 
+      local wait_time = 0
+      local ply = PlayerPedId() 
+      for k,v in pairs(polygon) do 
+        local firstPoint, secondPoint
+        if k == 1 then 
+          firstPoint = polygon[#polygon]
+          secondPoint = polygon[k]
+        else
+          firstPoint = polygon[k - 1]
+          secondPoint = polygon[k]
+        end
+
+        Core.Polygon.DrawWall(firstPoint, secondPoint, height)
+        DrawLine(firstPoint.x, firstPoint.y, i, secondPoint.x, secondPoint.y, i, 255, 0, 0, 255)
+      end
+
+      local hit, testCoords, entityHit = Core.UI.ScreenToWorld()
+      if (testCoords ~= vector3(0,0,0) and entityHit ~= ply) then
+        endCoords = testCoords 
+        DrawSphere(endCoords.x,endCoords.y,endCoords.z, 0.2, 255,0,0, 0.7)
+      end
+
+     
+      Core.UI.AdvancedHelpNotif("polygonPlotter", {
+        {
+          label = "Add Point",
+          key   = "g"
+        },
+        {
+          label = "Remove Last Point",
+          key   = "h"
+        },
+        {
+          label = "Finish Polygon",
+          key   = "x"
+        },
+        {
+          label = "Increase Height", 
+          key   = "⬆️"
+        },
+        {
+          label = "Decrease Height", 
+          key   = "⬇️"
+        },
+        {
+          label = "Cancel Polygon",
+          key   = "q"
+        }
+      })
+      DisableControlAction(0,47)
+      DisableControlAction(0,74)
+      DisableControlAction(0,105)
+      DisableControlAction(0,172)
+      DisableControlAction(0,173)
+      DisableControlAction(0,85)
+
+      if IsDisabledControlJustPressed(0, 47) then 
+        polygon[#polygon + 1] = endCoords
+      elseif IsDisabledControlJustPressed(0, 74) then
+        polygon[#polygon] = nil
+      elseif IsDisabledControlJustPressed(0, 105) then
+        return polygon
+      elseif IsDisabledControlPressed(0, 172) then
+        height = height + 0.05
+      elseif IsDisabledControlPressed(0, 173) then
+        height = height - 0.05
+      elseif IsDisabledControlJustPressed(0, 85) then
+        return false
+      end
+
+
+      Wait(wait_time)
+    end
+  end, 
 }
+
+RegisterCommand("testPoly", function(source,arg)
+  local polygon = Core.Polygon.newPolygon()
+  print(json.encode(polygon))
+end)
+
+
+
