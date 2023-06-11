b4cbed4111c54eeef8806bb7609db1f47b039df2
DirkDigglerz <liamsdargie@gmail.com>
2023-06-11 22:58:17 +0100

    Addition of muli-framework shared.lua

---
+++ usersettings/shared.lua
@@ -0,0 +1,29 @@
+Core.Shared = {}
+
+Core.Shared.Items = { --##
+  ['thermite'] = {
+    ['name']  = "thermite",
+    ['label'] = "Thermite",
+  }
+}
+
+Core.Shared.Vehicles = {
+  ['ballas'] = {
+    ['name']  = "blista",
+    ['label'] = "Blista",
+  }
+}
+
+Core.Shared.Jobs = {
+  ['ballas'] = {
+    ['name']  = "unemployed",
+    ['label'] = "Unemployed",
+  }
+}
+
+Core.Shared.Gangs = {
+  ['ballas'] = {
+    ['name']  = "ballas",
+    ['label'] = "Ballas",
+  }
+}
